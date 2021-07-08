8ba8641f266b2135599c35eaa8008ead93c1d13c
chayka <gabrielacpelaez@gmail.com>
2021-07-07 21:05:51 -0300

    primera version tarea 1 php

---
+++ tarea-php1.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tarea PHP 1</title>
+    <style> div {background-color: black;  width: max-content; padding: 30px; color:white;}   </style>
+
+</head>
+<body>
+    <div>
+        <?php
+            echo "<h1>" . "Gabriela Peláez" . "</h1>";
+            echo "<br>";
+            echo "Edad: 26 años";
+            echo "<br>";
+            echo "Hobbie: programar (por ahora)";
+            echo "<br>";
+            echo "Editor favorito: Visual Studio Code y Mu";
+            echo "<br>";
+            echo "Sistema operativo: Windows 10";
+        ?>
+    </div>
+</body>
+</html>
+
+
+
